47cd0d04e742a73d18dea8b1989e98998835b59e
Lameck1 <lameck72@gmail.com>
2021-05-05 14:10:22 +0300

    Implement Storage Logic

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function Book(title, author, pages, isbn, status) {
 function toggleStatus(book) {
   book.status = !book.status;
 }
+
+// Storage
+function getBooks() {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  return books;
+}
+
+const books = getBooks();
+
+function addBook(book) {
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+function statusToString(status) {
+  if (status) {
+    return 'Read';
+  }
+  return 'Unread';
+}
